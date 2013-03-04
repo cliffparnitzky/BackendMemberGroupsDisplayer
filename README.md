df92940ce988027ca91334efc1aecc1b1d18e8b1
Cliff Parnitzky <github@cliff-parnitzky.de>
2013-03-04 12:47:19 +0100

    Init

---
+++ README.md
@@ -0,0 +1,30 @@
+Contao Extension: BackendMemberGroupsDisplayer 
+==============================================
+
+Show member groups at members and pages in backend (to get a better overview of granted rights).
+
+
+Installation
+------------
+
+The extension is not published in contao extension repository.
+Install it manually.
+
+
+Tracker
+-------
+
+https://github.com/cliffparnitzky/BackendMemberGroupsDisplayer/issues
+
+
+Compatibility
+-------------
+
+- min. version: Contao 2.9.5
+- max. version: Contao 2.11.x
+
+
+Dependency
+----------
+
+- There are no dependencies to other extensions, that have to be installed.
---
+++ system/modules/zBackendMemberGroupsDisplayer/dca/tl_member.php
@@ -0,0 +1,58 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Cliff Parnitzky 2013
+ * @author     Cliff Parnitzky
+ * @package    BackendMemberGroupsDisplayer
+ * @license    LGPL
+ * @filesource
+ */
+
+// add new label callback
+$GLOBALS['TL_DCA']['tl_member']['list']['label']['label_callback'] = array('tl_member_BackendMemberGroupsDisplayer', 'setLabel');
+
+/**
+ * Class tl_member_BackendMemberGroupsDisplayer
+ *
+ * Provide miscellaneous methods that are used by the data configuration array.
+ * PHP version 5
+ * @copyright  Cliff Parnitzky 2013
+ * @author     Cliff Parnitzky
+ * @package    Controller
+ */
+class tl_member_BackendMemberGroupsDisplayer extends tl_member
+{
+	/**
+	 * Set the new label
+	 * @param array
+	 * @param string
+	 * @return string
+	 */
+	public function setLabel($row, $label)
+	{
+		$this->import("BackendMemberGroupsDisplayer");
+		return parent::addIcon($row, $label) . $this->BackendMemberGroupsDisplayer->getMemberGroupNames(deserialize($row['groups']));
+	}
+}
+
+?>
